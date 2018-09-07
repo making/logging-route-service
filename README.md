5836daf527b694808b11e83eea88586d3db74d07
Toshiaki Maki <tmaki@pivotal.io>
2018-09-07 14:22:22 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Logging Service
+
+CF [Route Service](https://docs.cloudfoundry.org/services/route-services.htmlgi) that logs http request and response including bodies.
+
+
+## Deploy
+
+```
+mvn clean package -DskipTests=true
+cf push
+cf create-user-provided-service logging-svc -r https://logging-svc.cfapps.io
+``` 
+
+## Bind the route service
+
+```
+cf bind-route-service cfapps.io logging-svc --hostname your-app
+```
+
+## Unbind the route service
+
+```
+cf unbind-route-service cfapps.io logging-svc --hostname your-app
+```
+
+## Test locally
+
+```
+$ curl localhost:8080 -d foo=bar -H "X-CF-Forwarded-Url: http://httpbin.org/post" -H "X-CF-Proxy-Metadata: a" -H "X-CF-Proxy-Signature: a"
+```
---
+++ src/main/java/am/ik/routeservice/App.java
@@ -0,0 +1,37 @@
+package am.ik.routeservice;
+
+import java.util.Optional;
+
+import org.slf4j.LoggerFactory;
+import reactor.ipc.netty.http.server.HttpServer;
+
+import org.springframework.http.server.reactive.HttpHandler;
+import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
+import org.springframework.web.reactive.function.server.HandlerStrategies;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+public class App {
+
+	static RouterFunction<ServerResponse> routes() {
+		return new LoggingHandler().route();
+	}
+
+	public static void main(String[] args) throws Exception {
+		long begin = System.currentTimeMillis();
+		int port = Optional.ofNullable(System.getenv("PORT")) //
+				.map(Integer::parseInt) //
+				.orElse(8080);
+		HttpServer httpServer = HttpServer.create("0.0.0.0", port);
+		httpServer.startRouterAndAwait(routes -> {
+			HttpHandler httpHandler = RouterFunctions.toHttpHandler(App.routes(),
+					HandlerStrategies.builder().build());
+			routes.route(x -> true, new ReactorHttpHandlerAdapter(httpHandler));
+		}, context -> {
+			long elapsed = System.currentTimeMillis() - begin;
+			LoggerFactory.getLogger(App.class).info("Started in {} seconds",
+					elapsed / 1000.0);
+		});
+	}
+}
